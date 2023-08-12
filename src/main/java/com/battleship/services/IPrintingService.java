2ac080733f052aee5f0ab84e12ba462e395ac221
Pawel Raciborski <wojak2845@gmail.com>
2023-08-12 21:51:42 +0200

    - creating message for player - creating ship menu

--- src/main/java/com/battleship/services/IPrintingService.java
+++ src/main/java/com/battleship/services/IPrintingService.java
@@ -1,6 +1,17 @@
 package com.battleship.services;
 
 public interface IPrintingService {
+
+    String MENU = """
+            CHOOSE SHIP:
+            %s.%4s [%s]/[%s]
+            %s.%4s [%s]/[%s]
+            %s.%4s [%s]/[%s]
+            %s.%4s [%s]/[%s]
+            GIVE NUMBER BETWEEN (1-5):\s""";
+
+    String GIVEN_NICKNAME = "NICKNAME: ";
+    String PREPARING_BOARD_FOR_PLAYER = "PREPARING CONFIG FOR PLAYER: %s";
     String NEW_LINE = "\n";
     String GIVE_POINT = "GIVE POINT COORDINATES: ";
     String prepareNickname();
--- src/main/java/com/battleship/services/PrintingService.java
+++ src/main/java/com/battleship/services/PrintingService.java
@@ -1,13 +1,18 @@
 package com.battleship.services;
 
+import com.battleship.model.WarShip;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrintingService implements IPrintingService{
     @Override
     public String prepareNickname() {
+        printMessage(GIVEN_NICKNAME);
+        String name = ScannerProvider.getScanner().next();
 
-        return null;
+        printMessage(PREPARING_BOARD_FOR_PLAYER.formatted(name));
+
+        return name;
     }
 
     @Override
@@ -26,6 +31,28 @@ public class PrintingService implements IPrintingService{
 
     @Override
     public void printMenu() {
+        String menu = MENU.formatted(
+                WarShip.ShipType.CARRIER.getQuantity(),
+                WarShip.ShipType.CARRIER.name(),
+                ShipCache.getShipsListSize(WarShip.ShipType.CARRIER),
+                WarShip.ShipType.CARRIER.getQuantity(),
 
+                WarShip.ShipType.BATTLESHIP.getQuantity(),
+                WarShip.ShipType.BATTLESHIP.name(),
+                ShipCache.getShipsListSize(WarShip.ShipType.BATTLESHIP),
+                WarShip.ShipType.BATTLESHIP.getQuantity(),
+
+                WarShip.ShipType.SUBMARINE.getQuantity(),
+                WarShip.ShipType.SUBMARINE.name(),
+                ShipCache.getShipsListSize(WarShip.ShipType.SUBMARINE),
+                WarShip.ShipType.SUBMARINE.getQuantity(),
+
+                WarShip.ShipType.DESTROYER.getQuantity(),
+                WarShip.ShipType.DESTROYER.name(),
+                ShipCache.getShipsListSize(WarShip.ShipType.DESTROYER),
+                WarShip.ShipType.DESTROYER.getQuantity()
+        );
+
+        printMessage(menu);
     }
 }
